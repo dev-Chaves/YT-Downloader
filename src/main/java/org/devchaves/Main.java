d640b7b516d0c212e6c719c592041ae4b582d9de
dev-Chaves <chavesprogrammer@gmail.com>
2025-04-23 22:36:30 -0300

    Lendo o input do terminal e 'executando' comandos básicos

--- src/main/java/org/devchaves/Main.java
+++ src/main/java/org/devchaves/Main.java
@@ -1,9 +1,41 @@
 package org.devchaves;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        //yt-dlp -x --audio-format mp3 <URL>
+        System.out.println("Copie a URL aqui: ");
 
+        Scanner sc1 = new Scanner(System.in);
+
+        String url = sc1.nextLine();
+
+        ProcessBuilder pb = new ProcessBuilder();
+
+        pb.command("yt-dlp", "-f", "--audio-format", "mp3", url);
+
+        try {
+            Process process = pb.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+
+            while ((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("Processo finalizado!" + exitCode);
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
