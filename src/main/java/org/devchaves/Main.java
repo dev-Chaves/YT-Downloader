a8520b1078776365eba3ac1c23c00fab8a166bf7
dev-Chaves <chavesprogrammer@gmail.com>
2025-04-23 23:29:17 -0300

    MVP via terminal pronto

--- src/main/java/org/devchaves/Main.java
+++ src/main/java/org/devchaves/Main.java
@@ -3,35 +3,50 @@ package org.devchaves;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        //yt-dlp -x --audio-format mp3 <URL>
+        Path downloadsDir = Paths.get("/home/jao/Projetos/YT-Downloader/downloads");
+
+        if (!Files.exists(downloadsDir)) {
+            try {
+                Files.createDirectories(downloadsDir);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
         System.out.println("Copie a URL aqui: ");
-
         Scanner sc1 = new Scanner(System.in);
-
         String url = sc1.nextLine();
 
-        ProcessBuilder pb = new ProcessBuilder();
+        ProcessBuilder pb = new ProcessBuilder(
+                "/usr/bin/yt-dlp",
+                "-P", downloadsDir.toString(),
+                "-x",
+                "--audio-format", "mp3",
+                url
+        );
 
-        pb.command("yt-dlp", "-f", "--audio-format", "mp3", url);
+        pb.redirectErrorStream(true); // Combina stdout e stderr
 
         try {
             Process process = pb.start();
-
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
             String line;
-
-            while ((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
 
             int exitCode = process.waitFor();
-            System.out.println("Processo finalizado!" + exitCode);
+            System.out.println("Download finalizado! Arquivo salvo em: " + downloadsDir);
+            System.out.println("Processo finalizado! Código de saída: " + exitCode);
 
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
